f4d52889555eeddcf3d262d0f64475f78275a79f
JARVIS20240 <mistrykaran2911@gmail.com>
2026-02-13 23:50:25 +0530

    end of ch-5

---
+++ Ch-5_Groupby/01_groupby.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = {
+    "Name": ['Ram', 'Shyam', 'Ghanshyam', 'Dhanshyam', 'Aditi', 'Jagdish', 'Raj', 'Simran'],
+    "Age" : [29,34,32,30,29,32,29,32],
+    "Salary": [50000, 60000,45000,52000, 49000, 70000,48000, 58000],
+    "Performance Score": [85,90,78,92,88,95,80,89]
+}
+df=pd.DataFrame(data)
+
+grouped = df.groupby('Age')["Salary"].sum()
+print(grouped)
---
+++ Ch-5_Groupby/03_comman-aggrigation.py
@@ -0,0 +1,17 @@
+import pandas as pd
+data = {
+    "Name": ['Ram', 'Shyam', 'Ghanshyam', 'Dhanshyam', 'Aditi', 'Jagdish', 'Raj', 'Simran'],
+    "Age" : [29,34,32,30,29,32,29,32],
+    "Salary": [50000, 60000,45000,52000, 49000, 70000,48000, 58000],
+    "Performance Score": [85,90,78,92,88,95,80,89]
+}
+df=pd.DataFrame(data)
+
+"""
+1- sum()
+2- mean()
+3- count()
+4- min()
+5- max()
+6- std()
+"""
